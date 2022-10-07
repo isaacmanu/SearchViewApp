ea3cef237b1886ee2cd37783d1f54bd05640bb4c
Isaac Giblin-McGrady <67371753+isaacmanu@users.noreply.github.com>
2022-10-07 21:01:09 +0100

    Update RiotApiService.kt

--- app/src/main/java/com/example/searchviewapp/network/RiotApiService.kt
+++ app/src/main/java/com/example/searchviewapp/network/RiotApiService.kt
@@ -21,7 +21,7 @@ private const val BASE_URL =
 private const val BASE_URL_EUROPE = "https://europe.api.riotgames.com"
 
 //Development API key, needs to be regenerated every 24 hours and is not to be used in a live product
-private const val API_KEY = "RGAPI-f305e835-385c-4427-abbb-089ba3a2bc1e"
+private const val API_KEY = "RGAPI-a8332ac6-ef1b-42f6-8dfd-15840a5bec64"
 
 //Moshi object to handle JSON response
 private val moshi = Moshi.Builder()
